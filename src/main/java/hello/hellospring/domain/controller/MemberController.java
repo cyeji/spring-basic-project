d8e08386a631346851599c9d62e22e7da8f5a6a6
yeji.jo <choyeji1591@gmail.com>
2023-05-03 18:44:36 +0900

    feat : 스프링 어노테이션을 이용한 의존관계 설정 추가

---
+++ src/main/java/hello/hellospring/domain/controller/MemberController.java
@@ -0,0 +1,18 @@
+package hello.hellospring.domain.controller;
+
+import hello.hellospring.domain.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController ( MemberService memberService ) {
+        this.memberService = memberService;
+    }
+
+
+}
---
+++ src/main/java/hello/hellospring/domain/service/MemberService.java
@@ -0,0 +1,49 @@
+package hello.hellospring.domain.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.domain.repository.MemberRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService ( MemberRepository memberRepository ) {this.memberRepository = memberRepository;}
+
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
+    public Long join ( Member member ) {
+        // 같은 이름이 있는 중복 회원은 안됨
+        validateDuplicateMember( member );
+        memberRepository.save( member );
+        return member.getId();
+    }
+
+    private void validateDuplicateMember ( Member member ) {
+        memberRepository.findByName( member.getName() ).ifPresent( m -> {
+            throw new IllegalStateException( "이미 존재하는 회원입니다." );
+        } );
+    }
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers () {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne ( Long memberId ) {
+        return memberRepository.findById( memberId );
+    }
+
+
+}
