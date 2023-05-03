c470bf0e23a5d572a406e5ee9cbcf85dd288fb78
yeji.jo <choyeji1591@gmail.com>
2023-05-04 00:55:14 +0900

    fix : SpringDataJpa 구현 변경

--- src/main/java/hello/hellospring/domain/SpringConfig.java
+++ src/main/java/hello/hellospring/domain/SpringConfig.java
@@ -1,30 +1,22 @@
 package hello.hellospring.domain;
 
-import hello.hellospring.domain.repository.JpaMemberRepository;
 import hello.hellospring.domain.repository.MemberRepository;
 import hello.hellospring.domain.service.MemberService;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig ( EntityManager em ) {
-        this.em = em;
+    public SpringConfig ( MemberRepository memberRepository ) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService () {
-        return new MemberService( memberRepository() );
+        return new MemberService( memberRepository );
     }
 
-    @Bean
-    public MemberRepository memberRepository () {
-        return new JpaMemberRepository( em );
-    }
 }
